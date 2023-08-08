7f00ff5929a9b95492354c36a3aefbafc936f498
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-08-09 01:23:32 +0300

    made sorting

--- phones/views.py
+++ phones/views.py
@@ -4,21 +4,75 @@ from phones.models import Phone
 def index(request):
     return redirect('catalog')
 
+# В каталоге необходимо добавить возможность менять порядок отображения товаров:
+# по названию в алфавитном порядке и по цене по убыванию и по возрастанию.
 
+# def alphabet_sort(request):
+#     template = 'catalog.html'
+#     alph_sort = request.GET['sort']
+#     phone_object = Phone.objects.order_by(alph_sort)
+#     context = {'phones': phone_object}
+#     return render(request, template, context)
+
+# def show_catalog(request):
+#     template = 'catalog.html'
+#     alph_sort = request.GET['sort']
+#     phone_object = Phone.objects.order_by(alph_sort)
+#     context = {'phones': phone_object}
+#     return render(request, template, context)
+
+# def min_price(request):
+#     template = 'catalog.html'
+#     alph_sort = request.GET['sort']
+#     if alph_sort == 'min_price':
+#         alph_sort = 'price'
+#         phone_object = Phone.objects.order_by(alph_sort)
+#         context = {'phones': phone_object}
+#         return render(request, template, context)
+#
+# def max_price(request):
+#     template = 'catalog.html'
+#     alph_sort = request.GET['sort']
+#     if alph_sort == 'min_price':
+#         alph_sort = '-price'
+#         phone_object = Phone.objects.order_by(alph_sort)
+#         context = {'phones': phone_object}
+#         return render(request, template, context)
+
+# def show_catalog(request):
+#
+#     template = 'catalog.html'
+#     phone_object = Phone.objects.all()
+#     context = {'phones': phone_object}
+#     return render(request, template, context)
 
 def show_catalog(request):
     template = 'catalog.html'
+    alph_sort = request.GET['sort']
     phone_object = Phone.objects.all()
-    context = {'phones': phone_object}
-    return render(request, template, context)
-
+    if alph_sort  - получить реквест в строку и если есть параметры...
+        # phone_object = Phone.objects.all()
+        context = {'phones': phone_object}
+        return render(request, template, context)
+    elif alph_sort == 'name':
+        phone_object = Phone.objects.order_by(alph_sort)
+        context = {'phones': phone_object}
+        return render(request, template, context)
+    elif alph_sort == 'max_price':
+        alph_sort = '-price'
+        phone_object = Phone.objects.order_by(alph_sort)
+        context = {'phones': phone_object}
+        return render(request, template, context)
+    elif alph_sort == 'min_price':
+        alph_sort = 'price'
+        phone_object = Phone.objects.order_by(alph_sort)
+        context = {'phones': phone_object}
+        return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     phones = Phone.objects.filter(slug = slug)
-    print(phones)
-    # context ={}
     for phone in phones:
         context = {'phone': phone}
     return render(request, template, context)
