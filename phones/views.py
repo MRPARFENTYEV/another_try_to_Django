4f818b95d4cc7c1b55efccb77445e7a4d7e2071a
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-08-11 15:28:06 +0300

    revorked with GET.get metod

--- phones/views.py
+++ phones/views.py
@@ -9,28 +9,56 @@ def show_catalog(request):
 
     phone_object = Phone.objects.all()
     indicator = str(request).split()[-1]
+    beta_sort = request.GET.get('sort')
 
-    if "'/catalog/'>" in indicator:
-
+    if beta_sort is None:
         context = {'phones': phone_object}
         return render(request, template, context)
-    elif indicator == "'/catalog/?sort=name'>":
-        alph_sort = request.GET['sort']
-        phone_object = Phone.objects.order_by(alph_sort)
+    elif beta_sort == "name":
+        phone_object = Phone.objects.order_by(beta_sort)
         context = {'phones': phone_object}
         return render(request, template, context)
-    elif indicator == "'/catalog/?sort=max_price'>":
+    elif beta_sort == "max_price":
+        beta_sort = '-price'
+        phone_object = Phone.objects.order_by(beta_sort)
+        context = {'phones': phone_object}
+        return render(request, template, context)
+    elif beta_sort == "min_price":
+        beta_sort = 'price'
+        phone_object = Phone.objects.order_by(beta_sort)
+        context = {'phones': phone_object}
+        return render(request, template, context)
 
-        alph_sort = '-price'
-        phone_object = Phone.objects.order_by(alph_sort)
-        context = {'phones': phone_object}
-        return render(request, template, context)
-    elif indicator == "'/catalog/?sort=min_price'>":
 
-        alph_sort = 'price'
-        phone_object = Phone.objects.order_by(alph_sort)
-        context = {'phones': phone_object}
-        return render(request, template, context)
+# def show_catalog(request):
+#     template = 'catalog.html'
+#
+#     phone_object = Phone.objects.all()
+#     indicator = str(request).split()[-1]
+#     beta_sort = request.GET.get('sort')
+#
+#     if "'/catalog/'>" in indicator:
+#
+#         context = {'phones': phone_object}
+#         return render(request, template, context)
+#     elif indicator == "'/catalog/?sort=name'>":
+#         alph_sort = request.GET['sort']
+#         print(beta_sort)
+#         phone_object = Phone.objects.order_by(alph_sort)
+#         context = {'phones': phone_object}
+#         return render(request, template, context)
+#     elif indicator == "'/catalog/?sort=max_price'>":
+#         print(beta_sort)
+#         alph_sort = '-price'
+#         phone_object = Phone.objects.order_by(alph_sort)
+#         context = {'phones': phone_object}
+#         return render(request, template, context)
+#     elif indicator == "'/catalog/?sort=min_price'>":
+#         print(beta_sort)
+#         alph_sort = 'price'
+#         phone_object = Phone.objects.order_by(alph_sort)
+#         context = {'phones': phone_object}
+#         return render(request, template, context)
 
 
 def show_product(request, slug):
